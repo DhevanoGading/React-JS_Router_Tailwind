182b904caddddb1bd7325dbf496de51daccf80ee
Dhevano Gading <dhevanogading@gmail.com>
2022-03-25 08:14:37 +0700

    first commit

---
+++ src/pages/Home.jsx
@@ -0,0 +1,12 @@
+function Home() {
+    return (
+        <div>
+            <h1 className="mb-4 text-xl font-semibold">Hello World!</h1>
+            <p className="font-light">Choose one of our programs here ...</p>
+            <div class="mt-2 p-4 border rounded-xl font-mono text-sm whitespace-pre-wrap">
+            </div>
+        </div>
+    );
+}
+
+export default Home;
